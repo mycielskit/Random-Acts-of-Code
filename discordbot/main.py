c92542bb73475bf5cb453e48380cc7dd376f30c9
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-05-10 21:16:55 +0200

    Create main.py

---
+++ discordbot/main.py
@@ -0,0 +1,50 @@
+# https://discord.com/oauth2/authorize?client_id=708638946179022878&scope=bot&permissions=8
+
+import discord
+
+token = open("token.txt", "r").read()
+
+client = discord.Client()
+Author = 'INSERT CODE HERE'
+Friend = 'INSERT CODE HERE'
+
+
+@client.event  # event decorator/wrapper
+async def on_ready():
+    activity = discord.Activity(name='for 1000 members!', type=discord.ActivityType.watching)
+    await client.change_presence(activity=activity, status=discord.Status.idle)
+    print(f"Beep, boop. Logged in as {client.user}")
+
+
+@client.event
+async def on_message(msg):
+    if msg.author.bot or msg.content[0] != '$':
+        if msg.author.bot:
+            server = client.get_guild(INSERT GUILD HERE)
+            if len(server.members) == 1004:
+                botcommands = client.get_channel(INSERT CHANNEL HERE)
+                await botcommands.send(f'{Author} {Friend}')
+                await botcommands.send(f'We\'ve reached 1000 members! :tada:')
+                await client.change_presence(status=discord.Status.invisible)
+                await client.close()
+        return
+    print(f'{msg.channel}: {msg.author}: {msg.author.name}: {msg.content}')
+    cmd = msg.content[1:]
+    if cmd == "exit":
+        botcommands = client.get_channel(624056396639502336)
+        await botcommands.send(f'{Author}')
+        await botcommands.send('Going offline. See ya!')
+        await client.change_presence(status=discord.Status.invisible)
+        await client.close()
+        return
+    elif cmd == "test":
+        await msg.channel.send(f'{Author}')
+        server = client.get_guild(523349543505362945)
+        botcommands = client.get_channel(624056396639502336)
+        await botcommands.send(len(server.members))
+        return
+
+
+if __name__ == '__main__':
+    secret = ''
+    client.run(token)
